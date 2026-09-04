03af88c1eded2e4ddc84dcdb213224650c527bfb
Ashley Yeo <ashleyyeodeen@gmail.com>
2026-09-04 17:30:28 +0800

    another

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-print("====================")
-print("Welcome Here")
-print("My first post")
-print("====================")
+username = input("Enter your username: ")
+age =input("Enter your age: ")
+category = input("Enter your Content category: ")
+
+print("\nInstagram Profile")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
