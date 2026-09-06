ca901b3359ef80a54c6aeaae890a5b7a89a9819b
AshleyYeo <ashleyyeodeen@gmail.com>
2026-09-06 17:41:13 +0800

    My First Post

--- hello.py
+++ hello.py
@@ -1,12 +1,4 @@
-username = input("Enter your username: ")
-age =int(input("Enter your age: "))
-category = input("Enter your Content category: ")
-
-print("\nInstagram Profile")
-print("====================")
-print("Username:", username)
-print("Age:", age)
-print("Category:", category)
-
-if age>40 and category == "fun":
-    print("You are old what is fun for you?")
+print("==============")
+print("Welcome Here")
+print("My First Post")
+print("==============")
