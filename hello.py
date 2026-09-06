83aff3fc171f17651615ead65e05839d34bc0cc0
AshleyYeo <ashleyyeodeen@gmail.com>
2026-09-06 17:46:18 +0800

    second post

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==============")
 print("Welcome Here")
 print("My First Post")
 print("==============")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
